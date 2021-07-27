5d32ec150f1a8b617f86bbd1dfb3e54312e85d6e
Quảng Ngô Văn <quangnv@vinsofts.net>
2021-07-27 10:33:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -492,7 +492,7 @@
           (n.type = "text/css"),
             (n.rel = "stylesheet"),
             (n.href =
-              "https://cdn.jsdelivr.net/gh/ngoquang1312-jinb/public-test@1.8/activeintent.css");
+              "https://cdn.jsdelivr.net/gh/ngoquang1312-jinb/public-test@1.9/activeintent.css");
           var a = document.querySelector("script");
           a.parentNode.insertBefore(n, a),
             window.hasOwnProperty("webkitSpeechGrammarList") &&
